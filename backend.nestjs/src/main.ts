7c2d949036c06b830e6a670eb5308013999947f2
Stone <stone.dummett@gmail.com>
2023-01-13 17:42:12 +0100

    Add swagger for the api documentation

--- backend.nestjs/src/main.ts
+++ backend.nestjs/src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session'
 import * as passport from 'passport';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+	.setTitle("ft_transcendence")
+	.setDescription("The ft_transcendence API description")
+	.setVersion("0.1")
+	.build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   app.setGlobalPrefix("api");
   app.use(session({
 	secret: "ashkjdhsadkjashdsa",
--- backend.nestjs/src/users/dto/create-user.dto.ts
+++ backend.nestjs/src/users/dto/create-user.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+	@ApiProperty()
 	email: string;
+
+	@ApiProperty()
 	displayName: string;
 }
