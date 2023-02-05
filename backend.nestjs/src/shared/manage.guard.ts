885a4b523ff1fd1715c6656b60c84a48f6bd9940
Stone <stone.dummett@gmail.com>
2023-02-05 14:49:37 +0100

    Just in case

--- backend.nestjs/src/shared/manage.guard.ts
+++ backend.nestjs/src/shared/manage.guard.ts
@@ -3,6 +3,8 @@ import { CanActivate, ExecutionContext } from "@nestjs/common";
 export class ManageGuard implements CanActivate {
 	async canActivate(context: ExecutionContext) {
 		const request = context.switchToHttp().getRequest();
+		if (!request.user)
+			return false;
 		if (request.user.id != request.params.id)
 			return false;
 		return true;
