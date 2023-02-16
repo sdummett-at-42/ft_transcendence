2c05e7efe2813a8db31359f9e68e14c545407df4
Stone <stone.dummett@gmail.com>
2023-02-16 17:13:01 +0100

    Changing the FriendsController router global beginning route to /friends Since we know how to get id of user by using the @Req() decorator, /user/:id is no more needed

--- backend.nestjs/src/modules/friends/friends.controller.ts
+++ backend.nestjs/src/modules/friends/friends.controller.ts
@@ -1,86 +1,77 @@
-import { Controller, Get, UseGuards, Param, ParseIntPipe, Post, Patch, Delete, Body } from "@nestjs/common";
+import { Controller, Get, UseGuards, Req, ParseIntPipe, Post, Patch, Delete, Body } from "@nestjs/common";
 import { AuthenticatedGuard } from "src/modules/auth/utils/authenticated.guard";
-import { ManageGuard } from "src/shared/manage.guard";
 import { ContentTypeGuard } from "src/shared/content-type.guard";
 import { FriendsService } from "./friends.service";
 import { FriendRequestService } from "./friend-request.service";
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { UserEntity }from "src/modules/users/entities/user.entity";
+import { UserEntity } from "src/modules/users/entities/user.entity";
 
-@Controller('users/:id/friends')
+@Controller('friends')
 export class FriendsController {
 	constructor(private readonly friends: FriendsService, private readonly friendRequest: FriendRequestService) { }
 
 	@Get()
 	@UseGuards(AuthenticatedGuard)
 	@ApiOkResponse({ type: UserEntity, isArray: true })
-	async getFriends(@Param('id', ParseIntPipe) id: number) {
-		return this.friends.findAllFriends(id);
+	async getFriends(@Req() request) {
+		return this.friends.findAllFriends(request.user.id);
 	}
 
 	@Delete()
 	@UseGuards(AuthenticatedGuard)
-	@UseGuards(ManageGuard)
 	@UseGuards(ContentTypeGuard)
 	@ApiOkResponse({ type: UserEntity })
 	async removeFriend(
-		@Param('id', ParseIntPipe) id: number,
+		@Req() request,
 		@Body('friendId') friendId: number,
 	) {
-		console.log("hello world")
-		return this.friends.removeFriend(id, friendId);
+		return this.friends.removeFriend(request.user.id, friendId);
 	}
 
 	@Post('requests')
 	@UseGuards(AuthenticatedGuard)
-	@UseGuards(ManageGuard)
 	@UseGuards(ContentTypeGuard)
 	@ApiCreatedResponse({ type: UserEntity })
 	async sendFriendRequest(
-		@Param('id', ParseIntPipe) id: number,
+		@Req() request ,
 		@Body('friendId') friendId: number,
 	) {
-		return this.friendRequest.sendFriendRequest(id, friendId);
+		return this.friendRequest.sendFriendRequest(request.user.id, friendId);
 	}
 
 	@Patch('requests')
 	@UseGuards(AuthenticatedGuard)
-	@UseGuards(ManageGuard)
 	@UseGuards(ContentTypeGuard)
 	@ApiOkResponse({ type: UserEntity })
 	async acceptFriendRequest(
-		@Param('id', ParseIntPipe) id: number,
+		@Req() request ,
 		@Body('friendId') friendId: number,
 	) {
-		return this.friendRequest.acceptFriendRequest(id, friendId);
+		return this.friendRequest.acceptFriendRequest(request.user.id, friendId);
 	}
 
 	@Delete('requests')
 	@UseGuards(AuthenticatedGuard)
-	@UseGuards(ManageGuard)
 	@UseGuards(ContentTypeGuard)
 	@ApiOkResponse({ type: UserEntity })
 	async declineFriendRequest(
-		@Param('id', ParseIntPipe) id: number,
+		@Req() request ,
 		@Body('friendId') friendId: number,
 	) {
-		return this.friendRequest.declineFriendRequest(id, friendId);
+		return this.friendRequest.declineFriendRequest(request.user.id, friendId);
 	}
 
 	@Get('requests/sended')
 	@UseGuards(AuthenticatedGuard)
-	@UseGuards(ManageGuard)
 	@ApiOkResponse({ type: UserEntity, isArray: true })
-	async getFriendRequests(@Param('id', ParseIntPipe) id: number) {
-		return this.friendRequest.getSendedFriendRequests(id);
+	async getFriendRequests(@Req() request ) {
+		return this.friendRequest.getSendedFriendRequests(request.user.id);
 	}
 
 	@Get('requests/received')
 	@UseGuards(AuthenticatedGuard)
-	@UseGuards(ManageGuard)
 	@ApiOkResponse({ type: UserEntity, isArray: true })
-	async getReceivedFriendRequests(@Param('id', ParseIntPipe) id: number) {
-		return this.friendRequest.getReceivedFriendRequests(id);
+	async getReceivedFriendRequests(@Req() request ) {
+		return this.friendRequest.getReceivedFriendRequests(request.user.id);
 	}
-
 }
