23e4b3632b7187e40f5338f4fdd561896597c830
Stone <stone.dummett@gmail.com>
2023-02-21 14:55:43 +0100

    Change AuthenticatedGuard to return UNAUTHORIZED if user is not authenticated

--- backend.nestjs/src/modules/auth/utils/authenticated.guard.ts
+++ backend.nestjs/src/modules/auth/utils/authenticated.guard.ts
@@ -1,8 +1,10 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { CanActivate, ExecutionContext, HttpException, HttpStatus } from "@nestjs/common";
 
 export class AuthenticatedGuard implements CanActivate {
 	async canActivate(context: ExecutionContext) {
 		const request = context.switchToHttp().getRequest();
-		return request.isAuthenticated();
+		if (request.isAuthenticated())
+			return true;
+		throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
 	}
 }
