8f5163f0dab2281f00c85e2dedd03beb43d7622e
Stone <stone.dummett@gmail.com>
2023-03-04 12:18:33 +0100

    Temporary commit that implement google-oauth to allow me using two differents accounts for chat tests

--- backend.nestjs/src/modules/auth/auth.module.ts
+++ backend.nestjs/src/modules/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { FortyTwoStrategy } from './utils/fortytwo.strategy';
+import { GoogleStrategy } from './utils/google.strategy';
 import { SessionSerializer } from './utils/Serializer';
 
 @Module({
@@ -16,6 +17,7 @@ import { SessionSerializer } from './utils/Serializer';
 	controllers: [AuthController],
 	providers: [
 		FortyTwoStrategy,
+		GoogleStrategy,
 		SessionSerializer,
 		{
 			provide: "AUTH_SERVICE",
