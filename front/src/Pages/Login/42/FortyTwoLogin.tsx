7e0a263514d991aad649a615cdade48c1b1c7428
Nyfero <gaetan.sap@gmail.com>
2023-03-22 11:53:38 +0100

    login 42 in progress

--- front/src/Pages/Login/42/FortyTwoLogin.tsx
+++ front/src/Pages/Login/42/FortyTwoLogin.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./FortyTwoLogin.css"
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons"
+import { faChevronLeft, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"
 import Logo42 from "../../../assets/42_Logo.png"
 
 export default function FortyTwoLogin() {
@@ -16,8 +16,25 @@ export default function FortyTwoLogin() {
                     </Link>
 
                     <img className="LoginSelector-invert-effect LoginSelector-logo" src={Logo42} alt="Logo-ecole-42" />
-                    <h3 className="LoginSelector-card-title">Se connecter</h3>
+                    <h3 className="Login-card-title">Se connecter avec un compte 42</h3>
+                        <div className="Login-form">
 
+                        <form>
+                            <div className="Login-username">
+                                Nom d'utilisateur:
+                                <input type="text" name="username" required placeholder="username" />
+                            </div>
+                            <div className="Login-password">
+                                Mot de passe:
+                                <input type="password" name="password" required placeholder="password" />
+                            </div>
+                            <div className="Login-submit">
+                                <input type="submit" value="Se connecter" />
+                            </div>
+                        </form>
+                        </div>
+                        {/* <FontAwesomeIcon icon={faEye} />
+                        <FontAwesomeIcon icon={faEyeSlash} /> */}
                 </div>
             </div>
         </div>
