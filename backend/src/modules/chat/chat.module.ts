7dcc73eef51a69bae5ebbea85750f7705fc0d130
Stone <stone.dummett@gmail.com>
2023-04-18 19:37:31 +0200

    Delete FileController that was for test

--- backend/src/modules/chat/chat.module.ts
+++ backend/src/modules/chat/chat.module.ts
@@ -1,13 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "nestjs-prisma";
 import { RedisModule } from "../redis/redis.module";
-import { ChatGateway } from "./chat.gateway";
 import { ChatService } from "./chat.service";
-import { FileController } from "./file.controller";
 
 @Module({
 	imports: [RedisModule, PrismaModule],
-	controllers: [FileController],
+	controllers: [],
 	providers: [ChatService],
 	exports: [ChatService],
 })
