e6d67cfbb43f4b97580761929803aa90627ef9c0
gsap <gaetan.sap@gmail.com>
2023-04-27 17:01:33 +0200

    end game css done

--- frontend/src/Pages/Game/Pong/Result/Result.tsx
+++ frontend/src/Pages/Game/Pong/Result/Result.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { Player } from "../../../../../../backend/src/modules/game/entities/game.entities"
 import "./Result.css" 
 
@@ -13,36 +13,91 @@ const Result = ({data} : ResultProps) => {
     const winner : Player = data[1];
     const loser : Player = data[2];
 
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let winnerinterval = null;
+    const winnerRef = useRef<HTMLHeadingElement>(null);
+	let loserinterval = null;
+	const loserRef = useRef<HTMLHeadingElement>(null);
+
+    // Handle name animation
+	const WinnerCascade = (event: React.MouseEvent<HTMLHeadingElement>) => {  
+		let iteration = 0;
+		
+		clearInterval(winnerinterval);
+				
+		winnerinterval = setInterval(() => {
+			if (!winnerRef.current) return;
+			winnerRef.current.innerText = winnerRef.current.dataset.value
+				.split("")
+				.map((letter, index) => {
+					if(index < iteration) {
+						return winnerRef.current?.dataset.value[index];
+					}
+					return letters[Math.floor(Math.random() * 26)]
+				})
+				.join("");
+					
+			if(iteration >= winnerRef.current.dataset.value.length) { 
+				clearInterval(winnerinterval);
+			}
+					
+			iteration += 1 / 6;
+		}, 30);
+	};
+	
+	// Handle succes animation
+	const LoserCascade = (event: React.MouseEvent<HTMLHeadingElement>) => {  
+		let iteration = 0;
+		
+		clearInterval(loserinterval);
+		
+		loserinterval = setInterval(() => {
+			if (!loserRef.current) return;
+			loserRef.current.innerText = loserRef.current.dataset.value
+				.split("")
+				.map((letter, index) => {
+					if(index < iteration) {
+						return loserRef.current?.dataset.value[index];
+					}
+					return letters[Math.floor(Math.random() * 26)]
+				})
+				.join("");
+		
+			if(iteration >= loserRef.current.dataset.value.length) { 
+				clearInterval(loserinterval);
+			}
+		
+			iteration += 1 / 6;
+		}, 30);
+	};
 
 
     return (
-    <div>
-        <div className="screenGlobal">
-            <div class="heading">
-                <h2>Fin de la partie par {victory} ! Le gagnant est {winner.name} !</h2>
-                <h2>{winner.score} - {loser.score}</h2>
-
-            </div>
-            
-            <div class="winner-loser-info">
-                <div class="winner-info">
-                   <h3>Gagnant</h3>
-                   {/* <p>{JSON.stringify(winner)}</p> */}
-                   <p>{winner.name}</p>
-                   <p>{winner.elo}</p>
-                   <p>{winner.eloChange}</p>
-                </div>
-                <div class="loser-info">
-                   <h3>Perdant</h3>
-                    {/* <p>{JSON.stringify(loser)}</p> */}
-                    <p>{loser.name}</p>
-                    <p>{loser.elo}</p>
-                    <p>{loser.eloChange}</p>
-                    
+        <div className="RoomCreate-screen-card">
+            <div className="RoomCreate-screen-card-overlay"></div>
+            <div className="RoomCreate-screen-card-content">
+			    <div className="RoomCreate-screen-card-content-body">
+                    <div className="heading">
+                        <h2 className='Profile-screen-card-text'>Fin de la partie par {victory} ! Le gagnant est {winner.name} !</h2>
+                        <h2 className='Profile-screen-card-title'>{winner.score} - {loser.score}</h2>
+                    </div>
+                    <div className="winner-loser-info Profile-screen-card-text">
+                        <div className="winner-info" onMouseOver={WinnerCascade}>
+                            <h3 className='winner-title'>Gagnant</h3>
+                            <p>{winner.name}</p>
+                            <p data-value={winner.elo + winner.eloChange} ref={winnerRef}>{winner.elo}</p>
+                            <p id="winner-elo">{winner.eloChange}</p>
+                        </div>
+                        <div className="loser-info" onMouseOver={LoserCascade}>
+                            <h3 className='loser-title'>Perdant</h3>
+                            <p>{loser.name}</p>
+                            <p data-value={loser.elo - loser.eloChange} ref={loserRef}>{loser.elo}</p>
+                            <p id="loser-elo">{loser.eloChange}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 };
 
